e7c00fcbac2351693be7de1abed37e239b84356f
Rodrigo Zavala <rodrigoztrejo@gmail.com>
2023-11-15 01:05:44 -0600

    [edited] Navbar.jsx improved styles and animations

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,58 @@
-import classes from './Navbar.module.css'
+import React, { useState } from 'react';
+import classes from './Navbar.module.css'; // Import your CSS module
 
 const Navbar = () => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => {
+        setIsNavCollapsed(!isNavCollapsed);
+    };
+
     return (
-        <nav className={classes.container}>
-            <h1 className={classes.colorRed}>Ecommerce - Retro Techno</h1>
-            <section className={classes.categories}>
-                <button>Cellphones</button>
-                <button>Tablets</button>
-                <button>Computers</button>
-            </section>
+        <nav className={`navbar navbar-expand-lg navbar-light bg-light ${isNavCollapsed ? classes.fadeOut : classes.fadeIn}`}>
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <b>Computech</b>
+                </a>
+                <button
+                    className={`navbar-toggler ${isNavCollapsed ? 'collapsed' : ''}`}
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded={!isNavCollapsed}
+                    aria-label="Toggle navigation"
+                    onClick={handleNavCollapse}
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${isNavCollapsed ? '' : 'show'}`} id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" onClick={handleNavCollapse}>
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" onClick={handleNavCollapse}>
+                                Shop
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" onClick={handleNavCollapse}>
+                                Services
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" onClick={handleNavCollapse}>
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
-    )
-}
+    );
+};
 
-
-export default Navbar
+export default Navbar;
