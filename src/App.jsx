f0d8fa5fc7649847b8a64a9bc52e2fd9de3236bd
Rodrigo Zavala <rodrigoztrejo@gmail.com>
2023-12-15 18:40:24 -0600

    [added] Search on API call render and ItemDetail Functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState, createContext} from "react";
 import Navegationbar from "./components/Navbar/Navbar.jsx";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
 import CategoriesData from "./components/CategoriesData/CategoriesData.jsx";
@@ -5,22 +6,29 @@ import MercadoLibre from "./components/MercadoLibre/MercadoLibre.jsx";
 import IncrementCount from "./components/IncrementCount/IncrementCount.jsx";
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.jsx";
+
+/*Initializing Context*/
+const Context = createContext()
 function App() {
+    /*Creating context*/
+
   return (
     <>
         <BrowserRouter>
-            {/*Showing always*/}
-            <div>
-                <Navegationbar/>
-            </div>
-            <br/>
-            <br/>
-            {/*Relaying on routes*/}
-            <Routes>
-                <Route path='/' element={<ItemListContainer item={'displaying item list'}/>}/>
-                <Route path='/detail/:productId/' element={<ItemDetailContainer/>}/>
-                <Route path='/*' element={<h1>404 Not Found</h1>}/>
-            </Routes>
+            <Context.Provider value={'Testing Value Sent from Context'}>
+                {/*Showing always*/}
+                <div>
+                    <Navegationbar/>
+                </div>
+                <br/>
+                <br/>
+                {/*Relaying on routes*/}
+                <Routes>
+                    <Route path='/' element={<ItemListContainer item={'displaying item list'}/>}/>
+                    <Route path='/detail/:productId/' element={<ItemDetailContainer/>}/>
+                    <Route path='/*' element={<h1>404 Not Found</h1>}/>
+                </Routes>
+            </Context.Provider>
         </BrowserRouter>
 
         <br/>
