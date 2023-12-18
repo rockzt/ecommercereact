967c0add98a3dbfb0d88d1da52604d660a3b96f0
Restyled.io <commits@restyled.io>
2023-12-18 05:46:18 +0000

    Restyled by prettier

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,30 +1,30 @@
-import {useState} from "react";
+import { useState } from "react";
 import Button from "../Button/Button.jsx";
 
-const ItemCount = ({initial = 1, stock, onAdd}) => {
-    const [count, setCount] = useState(initial)
-    const increment = () => {
-        if(count < stock){
-            setCount(prev => prev + 1)
-        } else {
-            alert(`Only ${stock} on stock`)
-        }
+const ItemCount = ({ initial = 1, stock, onAdd }) => {
+  const [count, setCount] = useState(initial);
+  const increment = () => {
+    if (count < stock) {
+      setCount((prev) => prev + 1);
+    } else {
+      alert(`Only ${stock} on stock`);
     }
-    const decrement = () => {
-        if (count > 1){
-            setCount(prev => prev - 1)
-        } else {
-            alert('Cannot add less than 1')
-        }
+  };
+  const decrement = () => {
+    if (count > 1) {
+      setCount((prev) => prev - 1);
+    } else {
+      alert("Cannot add less than 1");
     }
-    return(
-        <div>
-            <h4 style={{textAlign:"center"}}>Add: {count}</h4>
-            <Button callback={decrement} label={'-'}/>
-            <Button callback={onAdd} label={'Add to Cart'}/>
-            <Button callback={increment} label={'+'}/>
-        </div>
-    )
-}
+  };
+  return (
+    <div>
+      <h4 style={{ textAlign: "center" }}>Add: {count}</h4>
+      <Button callback={decrement} label={"-"} />
+      <Button callback={onAdd} label={"Add to Cart"} />
+      <Button callback={increment} label={"+"} />
+    </div>
+  );
+};
 
-export default ItemCount
+export default ItemCount;
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,44 +1,43 @@
 import React from "react";
-import {Link, useParams} from "react-router-dom";
-import {useState, useEffect} from "react";
-import {getProductById} from "../ItemListContainer/asyncMock.js";
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getProductById } from "../ItemListContainer/asyncMock.js";
 import Button from "../Button/Button.jsx";
 import styles from "./ItemDetailContainer.module.css";
 import ItemCount from "../ItemCount/ItemCount.jsx";
 
-
-
 const ItemDetailContainer = () => {
-    const [product, setProduct] = useState(null)
-    const {productId} = useParams()
+  const [product, setProduct] = useState(null);
+  const { productId } = useParams();
 
-    useEffect(() => {
-        getProductById(productId)
-            .then(response => {
-                setProduct(response)
-            })
-    }, [productId])
-    return(
-        <div className={styles.productDescription}>
-            <img src={product?.img} alt={product?.productName} className={styles.productImage} />
-            <div className={styles.productDetails}>
-                <h2 className={styles.productName}>{product?.productName}</h2>
-                <p className={styles.category}>Category: {product?.category}</p>
-                <p className={styles.quantity}>Quantity: {product?.qty}</p>
-                <p className={styles.sku}>SKU: {product?.sku}</p>
-            </div>
-            <ItemCount stock={product?.qty}></ItemCount>
-            {
-                /*
+  useEffect(() => {
+    getProductById(productId).then((response) => {
+      setProduct(response);
+    });
+  }, [productId]);
+  return (
+    <div className={styles.productDescription}>
+      <img
+        src={product?.img}
+        alt={product?.productName}
+        className={styles.productImage}
+      />
+      <div className={styles.productDetails}>
+        <h2 className={styles.productName}>{product?.productName}</h2>
+        <p className={styles.category}>Category: {product?.category}</p>
+        <p className={styles.quantity}>Quantity: {product?.qty}</p>
+        <p className={styles.sku}>SKU: {product?.sku}</p>
+      </div>
+      <ItemCount stock={product?.qty}></ItemCount>
+      {/*
             <Button label={'-'} callback={() => {console.log(`Product ID: ${productId} added to cart`)}}/>
             <input type="number"/>
             <Button label={'+'} callback={() => {console.log(`Product ID: ${productId} added to cart`)}}/>
             <br/>
             <Button label={'Add to cart'} callback={() => {console.log(`Product ID: ${productId} added to cart`)}}/>
 
-                 */
-            }
-        </div>
-    )
-}
-export default ItemDetailContainer
+                 */}
+    </div>
+  );
+};
+export default ItemDetailContainer;
